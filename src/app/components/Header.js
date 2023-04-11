7627e6e4fce7f505fab3c5d745ad1eb1ada957de
Dan Hyman <dahyman91@gmail.com>
2023-04-11 14:28:23 -0400

    WIP: Header and background

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+"use client"
+import 'tailwindcss/tailwind.css'
+function Header() {
+  return (
+    <div className="flex justify-center mt-14">
+        <a href='#' className="mx-28 border h-28">LUXURY SKIN LOGO</a>
+        <a href='#' className="mx-28 border h-28">ALBUM TITLE</a>
+        <a href='#' className="mx-28 border h-28">MERCH</a>
+    </div>
+  )
+}
+
+export default Header
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,14 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-import Navbar from './Navbar'
+import Header from './components/Header'
+import 'tailwindcss/tailwind.css'
+import './styles/global.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Navbar />
-      <div className={styles.center}>
-        <h1 className="text-3xl font-bold underline">LXS</h1>
-      </div>
-
-      <div className={styles.grid}>
-       
-      </div>
+    <main>
+      <Header />
     </main>
   )
 }
